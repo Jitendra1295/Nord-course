f6f75371e522dadee58d1039c057b8e291c2141a
jitendra <pateljitendrao55@gmail.com>
2024-02-16 14:22:01 +0530

    express with http method

--- app.js
+++ app.js
@@ -12,28 +12,68 @@ const courses = [{
 
 // get, post , put , delete
 
-app.get("/", (req, res) => {
-    res.send("Hello world")
-})
-
-app.get("/about", (req, res) => {
-    res.send("about page")
-})
-
-app.get("/contact", (req, res) => {
-    res.send("contact us a abcd@abcd.com")
-})
+app.use(express.json())
 
 // route parameters
 
-app.get("/user/:id", (req, res) => {
-    res.send(req.params.id)
-})
-
+//  get specific course
 app.get("/course/:id", (req, res) => {
     const course = courses.find(course => course.id === parseInt(req.params.id));
-    if (!course) res.status(404).send("data not found")
+    if (!course) res.status(404).send("Data not found")
     res.send(course)
+})
+
+//get all courses
+app.get("/courses", (req, res) => {
+    res.send(courses)
+})
+
+// add new course
+// create
+
+app.get("/courses", (req, res) => {
+    res.send(courses)
+})
+app.post("/courses", (req, res) => {
+    console.log("req.body::", req.body, req.body.name);
+    const courseName = req.body.name
+    if (typeof courseName !== 'string') return res.status(404).send("Bad Request")
+    const course = courses.find(course => course.name === courseName);
+    if (course) return res.status(400).send("Data Already Exists")
+    const addCourse = {
+        name: courseName,
+        id: courses.length + 1
+    }
+    courses.push(addCourse);
+    res.status(200).send({
+        msg: "Data Added",
+        ...courses
+    })
+})
+
+// put method
+app.put("/courses/:name", (req, res) => {
+    const courseName = req.params.name
+    const newName = req.body.name
+    if (typeof newName !== 'string') return res.status(404).send("Bad Request")
+    const course = courses.find(course => course.name === courseName);
+    course.name = newName;
+    res.status(201).send({
+        msg: "Data updated",
+        ...courses
+    })
+})
+
+// delete Method
+app.delete("/courses/:id", (req, res) => {
+    const course = courses.find(course => course.id === parseInt(req.params.id));
+    console.log("id", req.params.id, course);
+    if (!course) return res.status(404).send("Data not found");
+    const index = courses.indexOf(course)
+    console.log("index", index);
+    courses.splice(index, 1);
+    res.send(course)
+
 })
 
 const PORT = process.env.PORT || 8080
